f9aa0439469b6654ec3e1b333586e46152a4c71b
Andriy Luchko <tul33640@temple.edu>
2025-03-06 17:18:12 -0500

    database progress bar by file

--- create_database_button.py
+++ create_database_button.py
@@ -1,20 +1,19 @@
 import os
-from PyQt5.QtWidgets import QPushButton, QFileDialog, QDialog, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QPushButton, QFileDialog, QDialog, QLabel, QVBoxLayout, QProgressBar
 from PyQt5.QtCore import QThread, pyqtSignal
 from csv_to_database import create_database
 
 class DatabaseCreationThread(QThread):
-    # Signal to indicate the task is done and return the database path
     task_done = pyqtSignal(str)  # Emit the path to the created database
+    progress_updated = pyqtSignal(int)  # Emit progress updates
 
     def __init__(self, path_to_data):
         super().__init__()
         self.path_to_data = path_to_data
 
     def run(self):
-        # Perform the database creation task
-        db_path = create_database(self.path_to_data)  # Ensure this function returns the database path
-        self.task_done.emit(db_path)  # Signal that the task is done and pass the database path
+        db_path = create_database(self.path_to_data, self.progress_updated)  # Pass the progress signal
+        self.task_done.emit(db_path)
 
 class DatabaseButton(QPushButton):
     # Signal to notify when the database is created and return its path
@@ -41,15 +40,25 @@ class DatabaseButton(QPushButton):
         message_label = QLabel("Creating database... Please wait.")
         layout.addWidget(message_label)
 
+        # Add a progress bar
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setMinimum(0)
+        self.progress_bar.setMaximum(100)
+        layout.addWidget(self.progress_bar)
+
         self.loading_dialog.setLayout(layout)
 
         # Start the database creation process in a separate thread
         self.db_thread = DatabaseCreationThread(path_to_data)
         self.db_thread.task_done.connect(self.on_database_created)  # Connect to the task_done signal
+        self.db_thread.progress_updated.connect(self.update_progress)  # Connect to the progress_updated signal
         self.db_thread.start()
 
         # Show the loading dialog
         self.loading_dialog.exec_()
+
+    def update_progress(self, value):
+        self.progress_bar.setValue(value)
 
     def on_database_created(self, db_path):
         # Close the loading dialog
